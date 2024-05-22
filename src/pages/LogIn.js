2207a8acd101b2c45f8faae5c7179eedf2ee01ba
Vlad Lautaru <vlad.lautaru@student.upt.ro>
2024-05-22 09:10:14 +0300

    Link with back-end
    The font-end and the back-end were succesfully linked via GET HTTP
    requests. Only the front-end remains for this pages.

--- src/pages/LogIn.js
+++ src/pages/LogIn.js
@@ -1,14 +1,47 @@
 import React from 'react';
+import {useState} from "react";
+import axios from 'axios';
 
 const LogIn = () => {
+    const [username, setUsername] = useState('');
+    const [password, setPassword] = useState('');
+    const [data, setData] = useState(null);
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleClick = async (event) => {
+        event.preventDefault(); // Prevent the default form submission behavior
+
+        try {
+            const response = await axios.get('http://localhost:5000/login', {
+                params: {
+                    username,
+                    password,
+                },
+            });
+            setData(response.data.message);
+            if (response.data.message === 'Correct login') {
+                alert(`username: ${username} password: ${password}`)
+            }
+        } catch (error) {
+            alert('Error:' + error);
+        }
+    };
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleClick}>
                 <label>Username:
-                    <input type="text" id="username" name="username"/>
+                    <input type="text" id="username" name="username" value={username} onChange={handleUsernameChange}/>
                 </label><br/>
                 <label>Password:
-                    <input type="password" id="username" name="username"/>
+                    <input type="password" id="username" name="username" value={password} onChange={handlePasswordChange}/>
                 </label><br/>
                 <button type="submit">Log-in</button>
             </form>
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,14 +1,65 @@
 import React from 'react';
+import {useState} from "react";
+import axios from 'axios';
 
 const Register = () => {
+    const [username, setUsername] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [retype, setRetype] = useState('');
+    const [data, setData] = useState('');
+
+    const handleUsernameChange = (event) => {
+        setUsername(event.target.value);
+    };
+
+    const handleEmailChange = (event) => {
+        setEmail(event.target.value);
+    };
+
+    const handlePasswordChange = (event) => {
+        setPassword(event.target.value);
+    };
+
+    const handleRetypeChange = (event) => {
+        setRetype(event.target.value);
+    };
+
+    const handleClick = async (event) => {
+        event.preventDefault(); // Prevent the default form submission behavior
+
+        try {
+            const response = await axios.get('http://localhost:5000/register', {
+                params: {
+                    username,
+                    email,
+                    password,
+                    retype,
+                },
+            });
+            setData(response.data.message);
+            if (response.data.message === 'User registered successfully') {
+                alert(`username: ${username} email: ${email} password: ${password}`)
+            }
+        } catch (error) {
+            alert('Error:' + error);
+        }
+    };
+
     return (
         <div>
-            <form>
+            <form onSubmit={handleClick}>
                 <label>Username:
-                        <input type="text" id="username" name="username"/>
+                    <input type="text" id="username" name="username" value={username} onChange={handleUsernameChange}/>
+                </label><br/>
+                <label>Email:
+                    <input type="email" id="email" name="email" value={email} onChange={handleEmailChange}/>
                 </label><br/>
                 <label>Password:
-                    <input type="password" id="username" name="username"/>
+                    <input type="password" id="password" name="password" value={password} onChange={handlePasswordChange}/>
+                </label><br/>
+                <label>Retype password:
+                    <input type="password" id="retype" name="retype" value={retype} onChange={handleRetypeChange}/>
                 </label><br/>
                 <button type="submit">Register</button>
             </form>
